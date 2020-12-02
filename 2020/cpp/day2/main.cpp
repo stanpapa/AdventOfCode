5c6a75d1f8577a49868eee512ef83dd4ea21c360
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2020-12-02 08:39:29 +0100

    Finished Day 2 Part 1

--- 2020/cpp/day2/main.cpp
+++ 2020/cpp/day2/main.cpp
@@ -10,15 +10,35 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
+  std::vector<std::string> entries;
   // read input from file
-  std::string fname = std::string(argv[1]);
-  std::vector<int> entries;
-  std::ifstream f(fname);
-  std::string line;
-  while (getline(f, line)) {
-    entries.push_back(std::stoi(line));
+  {
+    std::string fname = std::string(argv[1]);
+    std::ifstream f(fname);
+    std::string line;
+    while (getline(f, line)) {
+      entries.push_back(line);
+    }
+    f.close();
   }
-  f.close();
 
+  int validPwds = 0;
+  // part 1
+  for (const std::string& entry : entries) {
+    size_t pos = entry.find("-");
+    int min = std::stoi(entry.substr(0,pos));
+    int max = std::stoi(entry.substr(pos+1,entry.find(" ")));
+    pos = entry.find(":");
+    char letter = entry.substr(pos-1,1)[0];
+    std::string pwd = entry.substr(pos+2, entry.size()-pos-2);
 
+    int count = 0;
+    for (const char& c : pwd) {
+      if (c == letter) count++;
+    }
+    if (count >= min && count <= max) validPwds++;
+  } // entry
+
+  std::cout << "Number of valid passwords: " << validPwds << "\n";
+ 
 } 
