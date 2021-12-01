979b5d4666b2aeb775b2159cb1898e41fc89f22b
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2021-12-01 07:53:43 +0100

    Finished Rust Day1 2021

--- 2021/rust/src/bin/day1.rs
+++ 2021/rust/src/bin/day1.rs
@@ -1,11 +1,35 @@
-use std::io::{Error};
+use std::io::Error;
 
 // use crate::utils;
 use aoc_2021_rust::utils::input;
 
 pub fn main() -> Result<(), Error> {
-  println!("Hello, day1!");
-  let input = input::read_input()?;
+    let input = input::read_input_as_ints()?;
 
-  Ok(())
+    // part 1
+    let mut increases = 0;
+    let mut last_number = input[0];
+    for i in 1..input.len() {
+        if input[i] > last_number {
+            increases += 1;
+        }
+        last_number = input[i];
+    }
+
+    println!("Part 1: {}", increases);
+
+    // part 2
+    increases = 0;
+    last_number = input[0] + input[1] + input[2];
+    for i in 1..input.len() - 2 {
+        let sum = input[i] + input[i + 1] + input[i + 2];
+        if sum > last_number {
+            increases += 1;
+        }
+        last_number = sum;
+    }
+
+    println!("Part 2: {}", increases);
+
+    Ok(())
 }
--- 2021/rust/src/utils/input.rs
+++ 2021/rust/src/utils/input.rs
@@ -1,10 +1,14 @@
 use std::{
     env,
-    fs,
-    io::Error,
+    fs::{self, File},
+    io::{
+        Error,
+        BufRead,
+        BufReader,
+    },
 };
 
-pub fn read_input() -> Result<String, Error> {
+pub fn read_input_as_string() -> Result<String, Error> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 { panic!("Input file is missing! ABORT.\n"); }
@@ -12,4 +16,22 @@ pub fn read_input() -> Result<String, Error> {
     // read input from file
     let filename = &args[1];
     Ok(fs::read_to_string(filename)?)
+}
+
+pub fn read_input_as_ints() -> Result<Vec<i32>, Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 { panic!("Input file is missing! ABORT.\n"); }
+
+    // read input from file
+    let file = File::open(&args[1])
+        .expect("File could not be found.");
+    let reader = BufReader::new(file);
+
+    let numbers : Vec<i32> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    Ok(numbers)
 }
