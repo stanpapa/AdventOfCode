70bb7aac2f9a0d7525c111b1c8276c267e1ba26f
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-12-02 17:57:35 +0100

    Cleaned up day 2, 2022 (Rust)

--- 2022/rust/src/bin/day2.rs
+++ 2022/rust/src/bin/day2.rs
@@ -1,76 +1,87 @@
-use std::io::Error;
+use std::{collections::HashMap, io::Error};
 
 use aoc_lib::io::input::Input;
 
-// A, X -> Rock
-// B, Y -> Paper
-// C, Z -> Scissors
-fn part_1(tournament: &str) -> u64 {
-    tournament.lines().fold(0, |acc: u64, l| {
-        if let Some(game) = l.split_once(' ') {
-            match game {
-                ("A", "X") => acc + 4, // 1 for rock,     3 for draw
-                ("A", "Y") => acc + 8, // 2 for paper,    6 for win
-                ("A", "Z") => acc + 3, // 3 for scissors, 0 for loss
-                ("B", "X") => acc + 1, // 1 for rock,     0 for loss
-                ("B", "Y") => acc + 5, // 2 for paper,    3 for draw
-                ("B", "Z") => acc + 9, // 3 for scissors, 6 for win
-                ("C", "X") => acc + 7, // 1 for rock,     6 for win
-                ("C", "Y") => acc + 2, // 2 for paper,    0 for loss
-                ("C", "Z") => acc + 6, // 3 for scissors, 3 for draw
-                _ => acc,
-            }
-        } else {
-            acc
-        }
-    })
-}
+/// Rust unfortunately has no native way to declare a const/static HashMap
+/// using HashMap::from(), since it's a non-const fn
 
-// A -> Rock
-// B -> Paper
-// C -> Scissors
-// X -> Loss
-// Y -> Draw
-// Z -> Win
-fn part_2(tournament: &str) -> u64 {
-    tournament.lines().fold(0, |acc: u64, l| {
-        if let Some(game) = l.split_once(' ') {
-            match game {
-                ("A", "X") => acc + 3, // 3 for scissors, 0 for loss
-                ("A", "Y") => acc + 4, // 1 for rock,     3 for draw
-                ("A", "Z") => acc + 8, // 2 for paper,    6 for win
-                ("B", "X") => acc + 1, // 1 for rock,     0 for loss
-                ("B", "Y") => acc + 5, // 2 for paper,    3 for draw
-                ("B", "Z") => acc + 9, // 3 for scissors, 6 for win
-                ("C", "X") => acc + 2, // 2 for paper,    0 for loss
-                ("C", "Y") => acc + 6, // 3 for scissors, 3 for draw
-                ("C", "Z") => acc + 7, // 1 for rock,     6 for win
-                _ => acc,
-            }
-        } else {
-            acc
-        }
-    })
+fn solve(tournament: &str, score_table: &HashMap<&str, u64>) -> u64 {
+    tournament
+        .lines()
+        .map(|l| {
+            score_table
+                .get(l)
+                .unwrap_or_else(|| panic!("Invalid move!"))
+        })
+        .sum()
 }
 
 fn main() -> Result<(), Error> {
     let inp = Input::new();
 
-    println!("part 1: {}", part_1(&inp.to_string()));
-    println!("part 2: {}", part_2(&inp.to_string()));
+    // A, X -> Rock
+    // B, Y -> Paper
+    // C, Z -> Scissors
+    let score_table_1: HashMap<&str, u64> = HashMap::from([
+        ("A X", 4), // 1 for rock,     3 for draw
+        ("A Y", 8), // 2 for paper,    6 for win
+        ("A Z", 3), // 3 for scissors, 0 for loss
+        ("B X", 1), // 1 for rock,     0 for loss
+        ("B Y", 5), // 2 for paper,    3 for draw
+        ("B Z", 9), // 3 for scissors, 6 for win
+        ("C X", 7), // 1 for rock,     6 for win
+        ("C Y", 2), // 2 for paper,    0 for loss
+        ("C Z", 6), // 3 for scissors, 3 for draw
+    ]);
+
+    // A -> Rock
+    // B -> Paper
+    // C -> Scissors
+    // X -> Loss
+    // Y -> Draw
+    // Z -> Win
+    let score_table_2: HashMap<&str, u64> = HashMap::from([
+        ("A X", 3), // 3 for scissors, 0 for loss
+        ("A Y", 4), // 1 for rock,     3 for draw
+        ("A Z", 8), // 2 for paper,    6 for win
+        ("B X", 1), // 1 for rock,     0 for loss
+        ("B Y", 5), // 2 for paper,    3 for draw
+        ("B Z", 9), // 3 for scissors, 6 for win
+        ("C X", 2), // 2 for paper,    0 for loss
+        ("C Y", 6), // 3 for scissors, 3 for draw
+        ("C Z", 7), // 1 for rock,     6 for win
+    ]);
+
+    println!("part 1: {}", solve(&inp.to_string(), &score_table_1));
+    println!("part 2: {}", solve(&inp.to_string(), &score_table_2));
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     #[test]
     fn day_2_part_1() {
+        let score_table: HashMap<&str, u64> = HashMap::from([
+            ("A X", 4), // 1 for rock,     3 for draw
+            ("A Y", 8), // 2 for paper,    6 for win
+            ("A Z", 3), // 3 for scissors, 0 for loss
+            ("B X", 1), // 1 for rock,     0 for loss
+            ("B Y", 5), // 2 for paper,    3 for draw
+            ("B Z", 9), // 3 for scissors, 6 for win
+            ("C X", 7), // 1 for rock,     6 for win
+            ("C Y", 2), // 2 for paper,    0 for loss
+            ("C Z", 6), // 3 for scissors, 3 for draw
+        ]);
+
         assert_eq!(
-            super::part_1(
+            super::solve(
                 r#"A Y
 B X
-C Z"#
+C Z"#,
+                &score_table
             ),
             15
         );
@@ -78,11 +89,24 @@ C Z"#
 
     #[test]
     fn day_2_part_2() {
+        let score_table: HashMap<&str, u64> = HashMap::from([
+            ("A X", 3), // 3 for scissors, 0 for loss
+            ("A Y", 4), // 1 for rock,     3 for draw
+            ("A Z", 8), // 2 for paper,    6 for win
+            ("B X", 1), // 1 for rock,     0 for loss
+            ("B Y", 5), // 2 for paper,    3 for draw
+            ("B Z", 9), // 3 for scissors, 6 for win
+            ("C X", 2), // 2 for paper,    0 for loss
+            ("C Y", 6), // 3 for scissors, 3 for draw
+            ("C Z", 7), // 1 for rock,     6 for win
+        ]);
+
         assert_eq!(
-            super::part_2(
+            super::solve(
                 r#"A Y
 B X
-C Z"#
+C Z"#,
+                &score_table
             ),
             12
         );
