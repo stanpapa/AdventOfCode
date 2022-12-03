f78e839c59c7b9ef6066cb2a8b0bf7640d37ec7b
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-12-03 09:21:22 +0100

    Finished day 3, part 2, 2022 (Rust)

--- 2022/rust/src/bin/day3.rs
+++ 2022/rust/src/bin/day3.rs
@@ -5,32 +5,49 @@ use std::str::FromStr;
 use aoc_lib::io::input::Input;
 
 struct Rucksack {
-    compartment_1: String,
-    compartment_2: String,
+    contents: String,
+    half: usize,
 }
 
 impl FromStr for Rucksack {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let half = s.len() / 2;
         Ok(Rucksack {
-            compartment_1: String::from(&s[0..half]),
-            compartment_2: String::from(&s[half..]),
+            contents: String::from(s),
+            half: s.len() / 2,
         })
     }
 }
 
 impl Rucksack {
     fn priority(&self) -> u32 {
-        let unique_1: HashSet<char> = HashSet::from_iter(self.compartment_1.chars());
-        let unique_2: HashSet<char> = HashSet::from_iter(self.compartment_2.chars());
+        let unique_1: HashSet<char> =
+            HashSet::from_iter(self.contents.as_str()[0..self.half].chars());
+        let unique_2: HashSet<char> =
+            HashSet::from_iter(self.contents.as_str()[self.half..].chars());
 
-        unique_2
+        unique_1
             .iter()
-            .filter(|c| unique_1.contains(c))
+            .filter(|c| unique_2.contains(c))
             .map(priority)
             .sum()
+    }
+
+    fn contains(&self, item: &char) -> bool {
+        self.contents.contains(*item)
+    }
+
+    fn items_in_common(&self, other: &Rucksack) -> HashSet<char> {
+        let mut items = HashSet::new();
+
+        self.contents.chars().for_each(|c| {
+            if other.contains(&c) {
+                items.insert(c);
+            }
+        });
+
+        items
     }
 }
 
@@ -46,10 +63,24 @@ fn priority(item: &char) -> u32 {
 fn part_1(rucksacks: &str) -> u32 {
     rucksacks
         .lines()
-        .map(|l| {
-            Rucksack::from_str(l)
-                .expect("Unable to parse Rucksack")
-                .priority()
+        .map(|l| Rucksack::from_str(l).unwrap().priority())
+        .sum()
+}
+
+fn part_2(rucksacks: &str) -> u32 {
+    rucksacks
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|group| {
+            let set1 = Rucksack::from_str(group[0])
+                .unwrap()
+                .items_in_common(&Rucksack::from_str(group[1]).unwrap());
+            let set2 = Rucksack::from_str(group[0])
+                .unwrap()
+                .items_in_common(&Rucksack::from_str(group[2]).unwrap());
+            let intersection = set1.intersection(&set2);
+            priority(intersection.last().unwrap())
         })
         .sum()
 }
@@ -58,6 +89,7 @@ fn main() -> Result<(), Error> {
     let inp = Input::new();
 
     println!("part 1: {}", part_1(&inp.to_string()));
+    println!("part 2: {}", part_2(&inp.to_string()));
 
     Ok(())
 }
@@ -66,11 +98,31 @@ fn main() -> Result<(), Error> {
 mod tests {
     #[test]
     fn day_3_part_1() {
-        assert_eq!(0, 0);
+        assert_eq!(
+            super::part_1(
+                r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#
+            ),
+            157
+        );
     }
 
     #[test]
     fn day_3_part_2() {
-        assert_eq!(0, 0);
+        assert_eq!(
+            super::part_2(
+                r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#
+            ),
+            70
+        );
     }
 }
