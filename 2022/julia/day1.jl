53362dbc9200356f9d413ab1cd9583039ef8ad1d
Stan Papadopoulos <stan.papadopoulos@gmail.com>
2022-12-15 14:32:56 +0100

    Finished day 1, 2022 (Julia)

---
+++ 2022/julia/day1.jl
@@ -0,0 +1,11 @@
+f = open(ARGS[1], "r")
+input = read(f, String)
+close(f)
+
+calories = Vector{Int64}()
+for line in split(input, "\n\n")
+    push!(calories, sum([parse(Int64, ss) for ss in split(line, "\n")]))
+end
+
+println("part 1: $(maximum(calories))")
+println("part 2: $(sum(first(sort!(calories, rev = true), 3)))")
